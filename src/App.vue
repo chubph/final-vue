f15be3075617f850cb2086fe154d454fa95a8223
Filippos Mantzios <mantzios_ph@hotmail.com>
2022-12-16 16:47:20 +0200

    fixed lines

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script>
 
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 import HomePage from "./views/HomePage/HomePage.vue";
 import Navbar from "./components/Navbar/Navbar.vue";
 import Footer from "./components/Footer/Footer.vue";
 
-import { useUserStore } from "@/stores/userStore";
+import {useUserStore} from "@/stores/userStore";
 
 export default defineComponent({
   components: {
@@ -16,21 +16,38 @@ export default defineComponent({
   },
   setup() {
     const userStore = useUserStore();
-    return { userStore };
+    return {userStore};
   }
 });
 
 document.addEventListener("DOMContentLoaded", () => {
   function openModal($el) {
-    $el.classList.add('is-active');  }
+    $el.classList.add('is-active');
+  }
+
   function closeModal($el) {
-    $el.classList.remove('is-active');  }
+    $el.classList.remove('is-active');
+  }
+
   function closeAllModals() {
     (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-      closeModal($modal);    });  }
+      closeModal($modal);
+    });
+  }
+
   (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');    $close.addEventListener('click', () => {      closeModal($target);    });  });
-  document.addEventListener('keydown', (event) => {    const e = event || window.event;    if (e.keyCode === 27) {     closeAllModals();   }  });  });
+    const $target = $close.closest('.modal');
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+    if (e.keyCode === 27) {
+      closeAllModals();
+    }
+  });
+});
 </script>
 
 <template>
--- src/views/HomePage/HomePage.vue
+++ src/views/HomePage/HomePage.vue
@@ -1,22 +1,21 @@
 <script>
+
 import {defineComponent} from "vue";
 import {useUserStore} from "@/stores/userStore";
 import Info from "@/components/Info/Info.vue";
 
 export default defineComponent({
   name: "HomePage",
-  components: { Info },
+  components: {Info},
 
   setup() {
     const userStore = useUserStore();
     return {userStore};
-
   },
 
   data() {
     return {
-      chosenGame:{
-      },
+      chosenGame: {},
       games: [],
       games2: [],
       games3: [],
@@ -27,25 +26,25 @@ export default defineComponent({
         name: "",
         image: "",
         rating: "",
-
       },
-
     }
   },
 
   methods: {
-    handleByClick(event){
+    handleByClick(event) {
       const modal = event.target.dataset.target
       const $target = document.getElementById(modal)
       $target.classList.add('is-active')
     },
-    handleInfoClick(game){
+
+    handleInfoClick(game) {
       console.log(game)
-      this.chosenGame=game
+      this.chosenGame = game
       console.log(this.chosenGame)
       this.handleByClick(event)
     },
-    addGame: async function(event) {
+
+    addGame: async function (event) {
       // console.log(event)
       // console.log(event.target.dataset.gameid)
       console.log(this.userStore.user.username)
@@ -70,9 +69,7 @@ export default defineComponent({
   },
 
   async mounted() {
-
     const url = "https://api.rawg.io/api/games?key=2fe636237e6942a1afa7b7e75c21acc8";
-
     const response = await fetch(url);
     const result = await response.json();
     const gameList = result.results;
@@ -98,21 +95,17 @@ export default defineComponent({
       if (i >= 5 && i < 10) {
         currentGames2.push(gameOne)
         console.log(gameOne)
-
       }
 
       if (i >= 10 && i < 15) {
         currentGames3.push(gameOne)
         console.log(gameOne)
-
       }
 
       if (i >= 15 && i < gameList.length) {
         currentGames4.push(gameOne)
         console.log(gameOne)
-
       }
-
     }
 
     this.games = currentGames;
@@ -122,22 +115,17 @@ export default defineComponent({
     console.log(currentGames3)
     this.games4 = currentGames4;
     console.log(currentGames4)
-
   }
 });
-
-
 </script>
 
 <template>
-
 
   <div class='container hero is-fullheight'>
     <header class="has-text-centered">
       <br>
       <br>
       <br>
-
 
       <section class="hero  is-small" id="purpleCol">
         <div class="hero-body">
@@ -160,13 +148,12 @@ export default defineComponent({
       <section class="container">
         <div class="columns features ">
 
-
+          <!--          COLUMN 1  -->
           <div class="column is-3 ">
-
             <span v-for="game in games">
               <br>
 
-            <div class="card is-shady is-small" id="cardCol">
+              <div class="card is-shady is-small" id="cardCol">
 
                 <div class="card-image">
                   <figure class="image is-5by3">
@@ -177,28 +164,40 @@ export default defineComponent({
                 <div class="card-content">
                   <div class="content" id="cardDetails">
                     <h4 class="has-text-white-bis">{{ game.name }}</h4>
-                    <p> <strong class="has-text-white-bis"> Rating: {{ game.rating }}
-                      <i class="fa fa-star" id="asteraki"></i> </strong></p>
-                    <span class="js-modal-trigger button is-link modal-button" data-target="modal-js-example" @click="handleInfoClick(game)" id="DarkBlue">Info</span>
+                    <p>
+                      <strong class="has-text-white-bis">
+                      Rating: {{ game.rating }}
+                      <i class="fa fa-star" id="asteraki"></i>
+                      </strong>
+                    </p>
+                    <span class="js-modal-trigger button is-link modal-button"
+                          data-target="modal-js-example"
+                          @click="handleInfoClick(game)"
+                          id="DarkBlue">
+                      Info
+                    </span>
                   </div>
                   <div>
-                   <span class="button is-link modal-button" data-target="modal-image2" :data-gameid="game.id" id="DarkBlue"
-                         v-if="userStore.isLoggedIn === true" @click="addGame">Add</span>
+                   <span class="button is-link modal-button"
+                         data-target="modal-image2"
+                         :data-gameid="game.id"
+                         id="DarkBlue"
+                         v-if="userStore.isLoggedIn === true"
+                         @click="addGame">
+                     Add
+                   </span>
                   </div>
                 </div>
-
-            </div>
-
+              </div>
             </span>
           </div>
 
-
+          <!--          COLUMN 2  -->
           <div class="column is-3 ">
-
             <span v-for="game in games2">
               <br>
 
-            <div class="card is-shady is-small" id="cardCol">
+              <div class="card is-shady is-small" id="cardCol">
 
                 <div class="card-image">
                   <figure class="image is-5by3">
@@ -207,29 +206,42 @@ export default defineComponent({
                   </figure>
                 </div>
                 <div class="card-content">
-                  <div class="content">
+                  <div class="content" id="cardDetails">
                     <h4 class="has-text-white-bis">{{ game.name }}</h4>
-                    <p> <strong class="has-text-white-bis"> Rating: {{ game.rating }}  <i class="fa fa-star"
-                                                                                          id="asteraki"></i> </strong></p>
-                    <span class="button is-link modal-button" data-target="modal-image2" id="DarkBlue">?</span>
+                    <p>
+                      <strong class="has-text-white-bis">
+                      Rating: {{ game.rating }}
+                      <i class="fa fa-star" id="asteraki"></i>
+                      </strong>
+                    </p>
+                    <span class="js-modal-trigger button is-link modal-button"
+                          data-target="modal-js-example"
+                          @click="handleInfoClick(game)"
+                          id="DarkBlue">
+                      Info
+                    </span>
                   </div>
                   <div>
-                   <span class="button is-link modal-button" data-target="modal-image2" :data-gameid="game.id" id="DarkBlue"
-                         v-if="userStore.isLoggedIn === true" @click="addGame">Add</span>
+                   <span class="button is-link modal-button"
+                         data-target="modal-image2"
+                         :data-gameid="game.id"
+                         id="DarkBlue"
+                         v-if="userStore.isLoggedIn === true"
+                         @click="addGame">
+                     Add
+                   </span>
                   </div>
                 </div>
-
-            </div>
-
+              </div>
             </span>
           </div>
 
+          <!--          COLUMN 3  -->
           <div class="column is-3 ">
-
             <span v-for="game in games3">
               <br>
 
-            <div class="card is-shady is-small" id="cardCol">
+              <div class="card is-shady is-small" id="cardCol">
 
                 <div class="card-image">
                   <figure class="image is-5by3">
@@ -238,29 +250,42 @@ export default defineComponent({
                   </figure>
                 </div>
                 <div class="card-content">
-                  <div class="content">
+                  <div class="content" id="cardDetails">
                     <h4 class="has-text-white-bis">{{ game.name }}</h4>
-                    <p> <strong class="has-text-white-bis"> Rating: {{ game.rating }}  <i class="fa fa-star"
-                                                                                          id="asteraki"></i> </strong></p>
-                    <span class="button is-link modal-button" data-target="modal-image2" id="DarkBlue">?</span>
+                    <p>
+                      <strong class="has-text-white-bis">
+                      Rating: {{ game.rating }}
+                      <i class="fa fa-star" id="asteraki"></i>
+                      </strong>
+                    </p>
+                    <span class="js-modal-trigger button is-link modal-button"
+                          data-target="modal-js-example"
+                          @click="handleInfoClick(game)"
+                          id="DarkBlue">
+                      Info
+                    </span>
                   </div>
                   <div>
-                   <span class="button is-link modal-button" data-target="modal-image2" :data-gameid="game.id" id="DarkBlue"
-                         v-if="userStore.isLoggedIn === true" @click="addGame">Add</span>
+                   <span class="button is-link modal-button"
+                         data-target="modal-image2"
+                         :data-gameid="game.id"
+                         id="DarkBlue"
+                         v-if="userStore.isLoggedIn === true"
+                         @click="addGame">
+                     Add
+                   </span>
                   </div>
                 </div>
-
-            </div>
-
+              </div>
             </span>
           </div>
 
+          <!--          COLUMN 4  -->
           <div class="column is-3 ">
-
             <span v-for="game in games4">
               <br>
 
-            <div class="card is-shady is-small" id="cardCol">
+              <div class="card is-shady is-small" id="cardCol">
 
                 <div class="card-image">
                   <figure class="image is-5by3">
@@ -269,39 +294,50 @@ export default defineComponent({
                   </figure>
                 </div>
                 <div class="card-content">
-                  <div class="content">
+                  <div class="content" id="cardDetails">
                     <h4 class="has-text-white-bis">{{ game.name }}</h4>
-                    <p> <strong class="has-text-white-bis"> Rating: {{ game.rating }}  <i class="fa fa-star"
-                                                                                          id="asteraki"></i> </strong></p>
-                    <span class="button is-link modal-button" data-target="modal-image2" id="DarkBlue">?</span>
+                    <p>
+                      <strong class="has-text-white-bis">
+                      Rating: {{ game.rating }}
+                      <i class="fa fa-star" id="asteraki"></i>
+                      </strong>
+                    </p>
+                    <span class="js-modal-trigger button is-link modal-button"
+                          data-target="modal-js-example"
+                          @click="handleInfoClick(game)"
+                          id="DarkBlue">
+                      Info
+                    </span>
                   </div>
                   <div>
-                   <span class="button is-link modal-button" data-target="modal-image2" :data-gameid="game.id" id="DarkBlue"
-                         v-if="userStore.isLoggedIn === true" @click="addGame">Add</span>
+                   <span class="button is-link modal-button"
+                         data-target="modal-image2"
+                         :data-gameid="game.id"
+                         id="DarkBlue"
+                         v-if="userStore.isLoggedIn === true"
+                         @click="addGame">
+                     Add
+                   </span>
                   </div>
                 </div>
-
-            </div>
-
+              </div>
             </span>
           </div>
 
         </div>
       </section>
-
-
     </header>
   </div>
 
   <div id="modal-js-example" class="modal">
     <div class="modal-background"></div>
     <div class="modal-content">
-    <div class="box">
-      <p>Info</p>
-      <Info :game="chosenGame"></Info>
+      <div class="box">
+        <p>Info</p>
+        <Info :game="chosenGame"></Info>
+      </div>
     </div>
-    </div>
-<button class="modal-close is-large" aria-label="close">Close</button>
+    <button class="modal-close is-large" aria-label="close">Close</button>
   </div>
 </template>
 
