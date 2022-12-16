f627257f1161f8d0befbbe9f7b035cfddb6e2c65
mar10s <kourouklismarios@gmail.com>
2022-12-16 16:37:12 +0200

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default defineComponent({
   components: {
     Footer,
     Navbar,
-    HomePage
+    HomePage,
 
   },
   setup() {
@@ -20,7 +20,17 @@ export default defineComponent({
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {  function openModal($el) {    $el.classList.add('is-active');  }  function closeModal($el) {    $el.classList.remove('is-active');  }  function closeAllModals() {    (document.querySelectorAll('.modal') || []).forEach(($modal) => {      closeModal($modal);    });  }  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {    const $target = $close.closest('.modal');    $close.addEventListener('click', () => {      closeModal($target);    });  });    document.addEventListener('keydown', (event) => {    const e = event || window.event;    if (e.keyCode === 27) {     closeAllModals();   }  });  });
+document.addEventListener("DOMContentLoaded", () => {
+  function openModal($el) {
+    $el.classList.add('is-active');  }
+  function closeModal($el) {
+    $el.classList.remove('is-active');  }
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);    });  }
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');    $close.addEventListener('click', () => {      closeModal($target);    });  });
+  document.addEventListener('keydown', (event) => {    const e = event || window.event;    if (e.keyCode === 27) {     closeAllModals();   }  });  });
 </script>
 
 <template>
