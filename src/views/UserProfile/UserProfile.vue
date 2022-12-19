d756f04690d65102c47d72134e23afa3eb75f9f4
kaggelopoul <konnieaggelopoulou@yahoo.gr>
2022-12-19 11:32:34 +0200

    profile game list added

--- src/views/UserProfile/UserProfile.vue
+++ src/views/UserProfile/UserProfile.vue
@@ -9,11 +9,77 @@ export default defineComponent({
     const userStore = useUserStore();
     return { userStore };
   },
+  data() {
+    return {
+      games: {
+        type: Array
+      }
+
+    }
+
+  },
+  methods: {
+    getGameList:async function() {
+      const apiUrlGetGame = "http://localhost:8080/api/game/" + this.userStore.user.username;
+      const response = await fetch(apiUrlGetGame,{
+        method: "GET",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+
+      });
+      const result = await response.json();
+      this.games=result
+      console.log(this.games);
+
+
+    }
+
+  },
+
+
 });
+
 </script>
 
 <template>
-  <div class='container hero is-fullheight'></div>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+
+  <p>Hello1</p>
+  <br/>
+  <button @click="getGameList">Game List</button>
+  <br/>
+  <br/>
+  <table>
+    <tr>
+      <th>Game ID</th>
+    </tr>
+    <br/>
+    <tr v-for="game in games">
+      <td>{{ game }}</td>
+    </tr>
+  </table>
+
+  <div class='container hero is-fullheight'>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+    <p>Hello2</p>
+  </div>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+
+  <p>Hello3</p>
+
+
+
 </template>
 
 
