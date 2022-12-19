9d580a79e566bfe6e528661b4620b04ec72c2477
kaggelopoul <konnieaggelopoulou@yahoo.gr>
2022-12-19 18:45:46 +0200

    info description added

--- src/components/Info/Info.vue
+++ src/components/Info/Info.vue
@@ -1,17 +1,56 @@
 <script>
 import {defineComponent} from "vue";
+import {useUserStore} from "@/stores/userStore";
 
 export default defineComponent({
   name: "Info",
   props: {
-    game: {}
+    game: {},
   },
+
+
   data() {
     return {
-      GameDetails: {}
+      gameInfo : {
+        description: "",
+      },
+      GameDetails: {},
     }
   },
-  methods: {}
+
+
+  watch: {
+    async game() {
+      const url = "https://api.rawg.io/api/games/" + this.game.id + "?key=2fe636237e6942a1afa7b7e75c21acc8";
+      const response = await fetch(url);
+      const result = await response.json();
+
+      const curGame = result;
+      this.gameInfo = curGame
+    }
+
+  },
+
+//vhtml
+
+  methods: {
+    addGame: async function (event) {
+      // console.log(event)
+      // console.log(event.target.dataset.gameid)
+      // console.log(this.userStore.user.username)
+      const apiUrlAddGame = "http://localhost:8080/api/game";
+      const bodyAddGame = JSON.stringify({
+        username: this.userStore.user.username,
+        gameid: event.target.dataset.gameid,
+      })
+    }
+  },
+
+  setup() {
+    const userStore = useUserStore();
+    return {userStore};
+  },
+
 
 });
 </script>
@@ -20,49 +59,55 @@ export default defineComponent({
 <template>
 
 
- <div>
-  <div class="card has-text-white-bis" id="cardCol" >
-    <div class="card-image">
-      <h1 class="title has-text-white-bis has-text-centered">{{game.name}}</h1>
+  <div>
+    <div class="card has-text-white-bis" id="cardCol">
+      <div class="card-image">
+        <h1 class="title has-text-white-bis has-text-centered">{{ game.name }}</h1>
 
-      <figure class="image is-4by3">
-        <div class="fade">
-          <div class="shadow">
-        <img :src="(`${game.image}`)" alt="Placeholder image">
+        <figure class="image is-4by3">
+          <div class="fade">
+            <div class="shadow">
+              <img :src="(`${game.image}`)" alt="Placeholder image">
+            </div>
+          </div>
+        </figure>
+
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="subtitle is-6 has-text-white-bis"> Initial Release Date:
+              <time :datetime="game.time">{{ game.released }}</time>
+            </p>
+            <p>
+              <strong class="has-text-white-bis">
+                Rating: {{ game.rating }}
+                <i class="fa fa-star" id="asteraki"></i>
+              </strong>
+            </p>
           </div>
         </div>
-      </figure>
+        <div class="content">
+          <u>Description:</u>
 
-    </div>
-    <div class="card-content" >
-      <div class="media">
-        <div class="media-content">
-          <p class="subtitle is-6 has-text-white-bis"> Initial Release Date: <time :datetime="game.time">{{ game.released }}</time></p>
-          <p>
-            <strong class="has-text-white-bis">
-              Rating: {{ game.rating }}
-              <i class="fa fa-star" id="asteraki"></i>
-            </strong>
-          </p>
+          <div>
+            {{ gameInfo.description }}
+          </div>
+
+          <br>
+
         </div>
+        <button class="button is-link modal-button fa fa-plus"
+                data-target="modal-image2"
+                :data-gameid="game.name"
+                id="LightBlueTextYellow"
+                v-if="userStore.isLoggedIn === true"
+                @click="addGame">
+          Add to Library
+        </button>
       </div>
-      <div class="content">
-        Bla bla bla bla bla
-<!--        <button class="modal-close is-large" aria-label="close">Close</button>-->
-        <br>
-
-      </div>
-              <button class="button is-link modal-button fa fa-plus"
-                      data-target="modal-image2"
-                      :data-gameid="game.id"
-                      id="LightBlueTextYellow">
-<!--                      v-if="userStore.isLoggedIn === false"-->
-<!--                      @click="addGame">-->
-                Add to Library
-              </button>
     </div>
   </div>
- </div>
 
 </template>
 
@@ -81,7 +126,11 @@ export default defineComponent({
 }
 
 @keyframes fadeIn {
-  0% { opacity: 0; }
-  80% { opacity: 2; }
+  0% {
+    opacity: 0;
+  }
+  80% {
+    opacity: 2;
+  }
 }
 </style>
